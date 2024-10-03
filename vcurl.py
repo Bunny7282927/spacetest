688e6e3d815d5a0f6437b4bd5b109ff19eaf7b25
Richit Bhat <107234358+Bunny7282927@users.noreply.github.com>
2024-10-03 12:59:37 +0530

    Update vcurl.py

--- vcurl.py
+++ vcurl.py
@@ -3,4 +3,6 @@ url = input("Send GET request to :")
 if url.startswith(('http://', 'https://')):
     print("Please retry the command without entering the protocol (http://, https://)")
 else:
-  requests.get(url)
+  req = requests.get(url)
+    print("Response:")
+    print(req)
